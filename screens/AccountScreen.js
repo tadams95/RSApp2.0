d6abe76e6b2ac7eecf230a25a803ab7d7e992c2e
tadams95 <tyrelleadams28@gmail.com>
2025-02-03 17:23:21 -0800

    chore: Update mediaTypes in pickImage function for AccountScreen.js / update packages

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -82,7 +82,7 @@ export default function AccountScreen({ setAuthenticated }) {
   const pickImage = useCallback(async () => {
     try {
       let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        mediaTypes: ["images", "videos"],
         allowsEditing: true,
         aspect: [4, 3],
         quality: 1,
