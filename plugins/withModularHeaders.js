c86ec60d5318e5120e1b01c3bc69826044b07317
tadams95 <tyrelleadams28@gmail.com>
2026-01-16 18:42:32 -0800

    fix(ios): remove static frameworks, add modular headers for Firebase compatibility

---
+++ plugins/withModularHeaders.js
@@ -0,0 +1,15 @@
+const { withPodfile } = require('expo/config-plugins');
+
+const withModularHeaders = (config) => {
+  return withPodfile(config, (config) => {
+    const podfileContent = config.modResults.contents;
+    // prevent multiple additions
+    if (!podfileContent.includes('use_modular_headers!')) {
+      // Add it near the top of the file
+      config.modResults.contents = `use_modular_headers!\n${podfileContent}`;
+    }
+    return config;
+  });
+};
+
+module.exports = withModularHeaders;
